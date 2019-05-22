00d8e510e6a77695c72e0134c2117e93286feb49
Dmytro Naydonov <dmytro.naydonov@sgdigital.com>
2019-05-22 17:43:12 +0200

    Homework #2

---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,23 @@
+public class Point {
+
+double x;
+double y;
+
+    public Point (double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p){
+        return Math.sqrt(Math.pow((this.x-p.x),2)+Math.pow((this.y-p.y),2));
+    }
+
+
+
+    public static void main(String[] args) {
+    Point p1 = new Point(0,1);
+    Point p2 = new Point(2,-2);
+        System.out.println(p1.distance(p2));
+    }
+
+}
