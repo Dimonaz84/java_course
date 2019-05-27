a3b3f08e93969fa12a0e27a5eec009b7c4d81846
Dmytro Naydonov <dmytro.naydonov@sgdigital.com>
2019-05-27 17:07:36 +0200

    Applied Lecture 2.9

---
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,18 @@
+package pl.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import pl.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase{
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("modifiedName", "modifiedHeader", "modifiedFooter"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+
+}
