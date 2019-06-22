1e34e9122a459500aca51bec3fd331ee3ab46834
Dimonaz <dmitry.n@pdffiller.com>
2019-06-22 13:47:20 +0200

    Lecture 6, part 2

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -4,16 +4,22 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
     WebDriver driver;
-
+    private final Properties properties;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
@@ -22,29 +28,42 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
     public void init() {
+        String target = System.getProperty("target", "local");
+        try {
+            properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+        } catch (IOException e){
+            System.out.println("Configuration file not found!");
+            System.exit(1);
+        }
 
         if (browser.equals(BrowserType.CHROME)) {
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
         } else if (browser.equals(BrowserType.FIREFOX)) {
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+            FirefoxOptions options = new FirefoxOptions();
+            options.setCapability("marionette", false);
+            driver = new FirefoxDriver(options);
         } else if (browser.equals(BrowserType.IEXPLORE)) {
             WebDriverManager.iedriver().setup();
-            driver = new InternetExplorerDriver();
+            DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+            capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+            capabilities.setCapability("requireWindowFocus", true);
+            driver = new InternetExplorerDriver(capabilities);
         } else if (browser.equals(BrowserType.SAFARI)) {
             driver = new SafariDriver();
         }
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.get("http://localhost/addressbook/index.php");
+        driver.get(properties.getProperty("web.baseUrl"));
         sessionHelper = new SessionHelper(driver);
         groupHelper = new GroupHelper(driver);
         navigationHelper = new NavigationHelper(driver);
         contactHelper = new ContactHelper(driver);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.username"), properties.getProperty("web.password"));
     }
 
     public void stop() {
--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,21 @@
 package pl.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import pl.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() {
@@ -19,4 +27,13 @@ public class TestBase {
         app.stop();
     }
 
+    @BeforeMethod (alwaysRun = true)
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Starting " + m.getName() + "with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Finishing " + m.getName());
+    }
 }
